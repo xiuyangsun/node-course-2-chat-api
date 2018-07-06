df53c8740a63b6a1e7044ac9af0a5f091f3d4b5e
Bobby Sun <xsun785@gmail.com>
2018-07-06 14:48:59 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect',function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    to:"judy@huanggou.com",
-    test:"Hay Jude"
-  });
 });
 
 socket.on('disconnect',function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:"Bobby@server.com",
-    text:"Hey, this is Bobby",
-    created:12345
-  });
+  socket.on('createMessage',(message)=>{
+    console.log("Created message",message);
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createAt:new Date().getTime()
+    });
 
-  socket.on('createMessage',(newMessage)=>{
-    console.log("Created message",newMessage)
   });
 
   socket.on('disconnect',(socket)=>{
